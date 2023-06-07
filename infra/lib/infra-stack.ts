34eaf0263326d4754fc21764820645e9b74c0dd3
James Hoegerl <james-hoegerl@pluralsight.com>
2023-06-07 13:00:43 -0700

    make playgroiund friendly

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -6,40 +6,50 @@ export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = ec2.Vpc.fromLookup(this, "Default", {
-      vpcId: process.env.VPC_ID,
+    // new virtual private cloud for our instance
+    const vpc = new ec2.Vpc(this, "GuitarPartsPickerVpc", {
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          name: "Public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
     });
 
+    // new security group for our instance
     const securityGroup = new ec2.SecurityGroup(this, "MySecurityGroup", {
       vpc,
       allowAllOutbound: true, // Allow outbound traffic to the internet
     });
 
+    // add a rule allowing http traffic
     securityGroup.addIngressRule(
       ec2.Peer.anyIpv4(),
       ec2.Port.tcp(80),
       "Allow HTTP access from anywhere"
     );
 
-    const instance = new ec2.Instance(this, "MyInstance", {
+    // add a rule allowing ssh traffic
+    // WARNING: DO NOT DO THIS FOR A PRODUCTION INSTANCE IN PS
+    // THIS IS FOR SANDBOX USAGE ONLY
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      "Allow HTTP access from anywhere"
+    );
+
+    // Create our instance for our rails app
+    const instance = new ec2.Instance(this, "GuitarPartsPickerRailsApp", {
       vpc,
       instanceType: new ec2.InstanceType("t2.micro"),
       machineImage: ec2.MachineImage.latestAmazonLinux2(),
       securityGroup,
       vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
       associatePublicIpAddress: true,
-      keyName: "jamesmbp",
-      blockDevices: [
-        {
-          deviceName: "/dev/xvda",
-          volume: ec2.BlockDeviceVolume.ebs(10, {
-            encrypted: true,
-            deleteOnTermination: true,
-          }),
-        },
-      ],
     });
 
+    // alll connections into 80 on the instance
     instance.connections.allowFromAnyIpv4(
       ec2.Port.tcp(80),
       "Allow inbound HTTP traffic"
